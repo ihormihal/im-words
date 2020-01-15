c33cdf17888bf57e0a1b234e62fb46750a3b1c29
Ihor Mykhalchenko <imykhalchenko@datas-tech.com>
2020-01-15 18:28:25 +0200

    new logic

--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -1,107 +1,116 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Router, Params } from '@angular/router'
-import { Word, CategoriesService} from './../services/categories.service'
+import { Word, CategoriesService } from './../services/categories.service'
 import { SpeachService } from './../services/speach.service'
 
 @Component({
-  selector: 'app-word',
-  templateUrl: './word.component.html',
-  styleUrls: ['./word.component.scss']
+	selector: 'app-word',
+	templateUrl: './word.component.html',
+	styleUrls: ['./word.component.scss']
 })
 export class WordComponent implements OnInit {
-  
-  constructor(
-    private categoriesService: CategoriesService, 
-    private speachService: SpeachService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) { }
 
-  category_id:number = null
-  loading:boolean = false
-  words: Word[] = []
-  progress:number = 0
-  currentIndex:number = -1
-  currentWord:Word
-  currentValue:string = ''
-  correctWord:string = ''
+	constructor(
+		private categoriesService: CategoriesService,
+		private speachService: SpeachService,
+		private route: ActivatedRoute,
+		private router: Router
+	) { }
 
-  isIncorrect:boolean = true
-  isFinal:boolean = false
+	category_id: number = null
+	loading: boolean = false
 
-  ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      this.category_id = params.category_id
-      this.fetchWords(params.category_id)
-    })
-  }
+	length: number = 0
+	words: Word[] = []
+	//wordsAwait: Word[] = []
 
-  fetchWords(category_id: number) {
-    this.loading = true
-    this.categoriesService.fetchCategory(category_id)
-      .subscribe(category => {
-        this.words = this.shuffle(category.words)
-        this.loading = false
-        this.next()
-      })
-  }
+	progress: number = 0
+	// currentIndex: number = -1
+	currentWord: Word
+	currentValue: string = ''
+	correctWord: string = ''
 
-  shuffle(words: Word[]): Word[] {
-    return words.sort(() => Math.random() - 0.5)
-  }
+	isIncorrect: boolean = true
+	isFinal: boolean = false
 
-  speakWord(word: string) {
-    this.speachService.speak(word)
-  }
+	ngOnInit() {
+		this.route.params.subscribe((params: Params) => {
+			this.category_id = params.category_id
+			this.fetchWords(params.category_id)
+		})
+	}
 
-  check(full: boolean = false) {
-    if(full){
-      return this.currentValue.toLowerCase() === this.currentWord.word.toLowerCase()
-    }else{
-      return this.currentWord.word.toLowerCase().indexOf(this.currentValue.toLowerCase()) !== 0
-    }
-  }
+	fetchWords(category_id: number) {
+		this.loading = true
+		this.categoriesService.fetchCategory(category_id)
+			.subscribe(category => {
+				this.length = category.length
+				this.words = category.words
+				this.loading = false
+				this.next()
+			})
+	}
 
-  next() {
-    this.isFinal = false
-    this.isIncorrect = false
+	shuffle(): void {
+		this.words = this.words.sort(() => Math.random() - 0.5)
+	}
 
-    this.correctWord = ''
-    this.currentValue = ''
-    this.currentIndex++
-    if(this.currentIndex < this.words.length){
-      this.currentWord = this.words[this.currentIndex]
-    }else{
-      this.speachService.cancel()
-      this.router.navigate([`/category`, this.category_id])
-    }
+	speakWord(word: string) {
+		this.speachService.speak(word)
+	}
 
-    this.progress = this.currentIndex*100/this.words.length
-    
-  }
+	check(full: boolean = false) {
+		if (full) {
+			return this.currentValue.toLowerCase() === this.currentWord.word.toLowerCase()
+		} else {
+			return this.currentWord.word.toLowerCase().indexOf(this.currentValue.toLowerCase()) !== 0
+		}
+	}
 
-  onEnter(event: KeyboardEvent) {
-    if(event.code === 'Enter'){
+	next(remove: boolean = false) {
+		this.isFinal = false
+		this.isIncorrect = false
+		this.correctWord = ''
+		this.currentValue = ''
 
-      if(this.correctWord){
-        this.next()
-        return
-      }
+		if(remove) this.words.splice(0, 1)
+		if(this.words.length == 0){
+			this.speachService.cancel()
+			this.router.navigate([`/category`, this.category_id])
+		}
+		this.shuffle()
 
-      if(this.isFinal) {
-        this.speachService.speak(this.currentWord.word)
-        this.next()
-      }else{
-        this.speachService.speak(this.currentWord.word)
-        this.correctWord = this.currentWord.word
-      }
 
-    }else{
-      this.currentValue = (<HTMLInputElement>event.target).value
-      this.isIncorrect = this.check()
-      this.isFinal = this.check(true)
-    }
-    
-  }
+		this.currentWord = this.words[0]
+		this.progress = 100 * (1 - this.words.length / this.length)
+
+	}
+
+	onEnter(event: KeyboardEvent) {
+		if (event.code === 'Enter') {
+
+			//if hint
+			if (this.correctWord) {
+				this.next()
+				return
+			}
+
+			if (this.isFinal) {
+				//if correct -> just remove and carry on
+				this.speachService.speak(this.currentWord.word)
+				this.next(true)
+			} else {
+				//if incorrect -> show hint and to nothing
+				this.speachService.speak(this.currentWord.word)
+				this.correctWord = this.currentWord.word
+			}
+
+		} else {
+			this.currentValue = (<HTMLInputElement>event.target).value
+			this.isIncorrect = this.check()
+			this.isFinal = this.check(true)
+		}
+
+	}
 
 }
